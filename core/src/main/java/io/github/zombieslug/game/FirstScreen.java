3056ddcfd122f298c7f7a739adae5def9d889689
Didanrama <didanramadhan035@gmail.com>
2026-02-20 21:17:29 +0700

    Add testing background clear color to FirstScreen and simplify lifecycle methods

--- core/src/main/java/io/github/zombieslug/game/FirstScreen.java
+++ core/src/main/java/io/github/zombieslug/game/FirstScreen.java
@@ -1,45 +1,25 @@
 package io.github.zombieslug.game;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
 
-/** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+
     @Override
     public void show() {
-        // Prepare your screen here.
+
     }
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+        ScreenUtils.clear(0.3f, 0.6f, 0.9f, 1f); 
+
     }
 
-    @Override
-    public void resize(int width, int height) {
-        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
-        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
-        if(width <= 0 || height <= 0) return;
-
-        // Resize your screen here. The parameters represent the new window size.
-    }
-
-    @Override
-    public void pause() {
-        // Invoked when your application is paused.
-    }
-
-    @Override
-    public void resume() {
-        // Invoked when your application is resumed after pause.
-    }
-
-    @Override
-    public void hide() {
-        // This method is called when another screen replaces this one.
-    }
-
-    @Override
-    public void dispose() {
-        // Destroy screen's assets here.
-    }
+    @Override public void resize(int width, int height) {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+    @Override public void dispose() {}
 }
